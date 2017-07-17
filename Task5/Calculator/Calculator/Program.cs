e060b20835c6facc1d0a81aa281b83e8d5e33826
sab04 <swati.a.bhatia@gmail.com>
2017-07-17 17:16:45 +0530

    Task 5

---
+++ Task5/Calculator/Calculator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumResult = 0;
+            int num = 0;
+            bool correctInt = false;
+            List<string> list = new List<string>();
+            char[] separator = { '\n' }; //Initialized
+            string s = "";
+            string regEx = @"^\\\\.?\\\\$";
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Incorrect Arguments. Please enter arguments in this format: Sum 2 3");
+                sumResult = 0;
+            }
+            else if(args.Length > 0)
+            {
+                for (int i = 1; i < args.Length; i++)
+                {
+                    //Find the delimiter
+                    if(Regex.Match(args[i], regEx).Success)
+                    {
+                        s = args[i].Substring((args[i].IndexOf("\\\\") + 2), 1).ToString();
+                        separator = s.ToArray();
+                        break;
+                    }                  
+                }
+                for (int i = 2; i < args.Length; i++)
+                {                    
+                    if (args[i].Contains(s))
+                    {
+                        list = args[i].Split(separator, StringSplitOptions.RemoveEmptyEntries).ToList();
+                        foreach (var l in list)
+                        {
+                            correctInt = int.TryParse(l, out num);
+                            if (correctInt)
+                            {
+                                if(int.Parse(l) > 0)
+                                {
+                                    sumResult = sumResult + int.Parse(l);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error: Negative numbers are not allowed.");
+                                    break;
+                                }
+                            }
+                        }                        
+                    }
+                    else
+                    {
+                        correctInt = int.TryParse(args[i], out num);
+                        if (correctInt)
+                        {
+                            if (int.Parse(args[i]) > 0)
+                            {
+                                sumResult = sumResult + int.Parse(args[i]);
+                            }
+                            else
+                            {                         
+                                Console.WriteLine("Error: Negative numbers are not allowed.");
+                            }
+                        }
+                    }   
+                }
+                Console.WriteLine("Sum = " + sumResult);
+            }
+            
+            Console.WriteLine("Press any key to continue....");
+            Console.ReadLine();
+        }
+    }
+}
