ec87495ccd89e623bd3c7e5a612e55e39a94fd23
sab04 <swati.a.bhatia@gmail.com>
2017-07-17 17:13:45 +0530

    Task 1

---
+++ Task1/Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumResult = 0;
+            int num1 = 0;
+            int num2 = 0;
+            bool correctInt1 = false;
+            bool correctInt2 = false;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Incorrect Arguments. Please enter arguments in this format: Sum 2 3");
+                sumResult = 0;
+            }
+            else if (args.Length == 1)
+            {
+                sumResult = 0;
+            }
+            else if (args.Length == 2)
+            {
+                correctInt1 = int.TryParse(args[1], out num1);
+                if (correctInt1)
+                    sumResult = int.Parse(args[1]);
+            }
+            else if (args.Length == 3)
+            {
+                correctInt1 = int.TryParse(args[1], out num1);
+                correctInt2 = int.TryParse(args[2], out num2);
+                if (correctInt1 && correctInt2)
+                    sumResult = int.Parse(args[1]) + int.Parse(args[2]);
+                else if (correctInt1)
+                    sumResult = int.Parse(args[1]);
+                else
+                    sumResult = int.Parse(args[2]);
+            }
+            Console.WriteLine("Sum = " + sumResult);
+            Console.WriteLine("Press any key to continue....");
+            Console.ReadLine();
+        }
+    }
+}
